83bb4c422da331c7d94fe8ef23c3433ec644cfbb
gallowst <gallows@gallows.pw>
2024-02-09 17:27:23 +0000

    README

--- README.md
+++ README.md
@@ -2,4 +2,33 @@
 
 > Simple containerised Azure Function written in PowerShell
 
-- Note: It's massive, the base image needs selecting carefully
+- It's a massive Windows image, the base image needs selecting carefully and I need to look for a smaller one - nano server maybe?
+- Creates a Containerised Linux PowerShell Function
+- Can be deployed on a local Docker engine or in a Web App - other container engines will probably work but I've not tried :)
+- Function is called via a Web Browser or via curl/Invoke-WebConnection - Hit the Web App endpoint and append `api/chuck` to the URL
+- Call it without a parameter to get a Chuck joke from the CMDB in the `dev` category
+- Call it with one of the parameters to get a Chuck joke from a specific category by appending `?name=sport` to the URL
+
+~~~bash
+curl https://azurefunctionwebapp.azurewebsites.net/api/chuck?name=dev
+Category: dev - Chuck Norris can spawn threads that complete before they are started.
+~~~
+
+## Categories
+
+- animal
+- career
+- celebrity
+- dev
+- explicit
+- fashion
+- food
+- history
+- money
+- movie
+- music
+- political
+- religion
+- science
+- sport
+- travel
--- chuck/run.ps1
+++ chuck/run.ps1
@@ -7,15 +7,15 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
+$name = ($Request.Query.Name)
 
-if (-not ($name -in @("animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"))) {
+if (-not ($name.ToLower() -in @("animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"))) {
     $name = "dev"
 }
 
-if ($name) {
+if ($name.ToLower()) {
     $strJoke = ((Invoke-WebRequest "https://api.chucknorris.io/jokes/random?category=$name").Content | ConvertFrom-Json | Select-Object -Property Value).Value
-    $body = ("You asked for it: {0}" -f $strJoke)
+    $body = ("Category: {0} - {1}" -f $name.ToLower(),$strJoke)
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
@@ -23,7 +23,3 @@ Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
     Body = $body
 })
-
-<#
-["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
-#>
